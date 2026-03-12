64c575a512da56f049c67f7903c78ce310dbc517
marta <m.nowak033@gmail.com>
2026-03-12 15:34:05 +0100

    Remove logging

--- app/ui/DrinksList.tsx
+++ app/ui/DrinksList.tsx
@@ -6,8 +6,6 @@ import { Drink } from "../types";
 const DrinksList = ({ drinks }: { drinks: Promise<{ drinks: Drink[] }> }) => {
   const drinksList = use(drinks).drinks;
 
-  console.log("drinksList", drinksList);
-
   return <div>DrinksList</div>;
 };
 
