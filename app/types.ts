ab999546691f942318de86b231f31c2c951bc1b4
marta <m.nowak033@gmail.com>
2026-03-17 15:26:08 +0100

    Move redux hooks to their own file

--- app/types.ts
+++ app/types.ts
@@ -1,10 +1,3 @@
-import { useDispatch, useSelector, useStore } from "react-redux";
-import { AppDispatch, AppStore, RootState } from "./store";
-
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-export const useAppSelector = useSelector.withTypes<RootState>();
-export const useAppStore = useStore.withTypes<AppStore>();
-
 export interface Drink {
   dateModified: string | null;
   idDrink: string;
